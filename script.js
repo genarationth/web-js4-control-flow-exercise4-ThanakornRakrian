5431fc871024a0e051b3befc501bb9a0dcba971c
thanakorn <thanakornrakrian@gmail.com>
2024-05-14 11:29:51 +0700

    js4 control flow exercise4

---
+++ script.js
@@ -0,0 +1,10 @@
+let dayNumber = 2;
+
+dayNumber === 0 ? console.log('Sunday'):
+dayNumber === 1 ? console.log('Monday'):
+dayNumber === 2 ? console.log('Tuesday'):
+dayNumber === 3 ? console.log('Wednesday'):
+dayNumber === 4 ? console.log('Thursday'):
+dayNumber === 5 ? console.log('Friday'):
+dayNumber === 6 ? console.log('Saturday'):
+console.log("We only have seven days a week. Create your day");
